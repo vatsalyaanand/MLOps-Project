46160c20e1f2f000bf92a4699a343465869f7658
Vatsalya Anand <vatsalya@buffalo.edu>
2024-02-29 15:16:01 -0500

    Prediction Pipline

--- src/utils.py
+++ src/utils.py
@@ -20,6 +20,7 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
+
 def evaluate_models(X_train,y_train,X_test,y_test,models):
     try:
         report ={}
@@ -33,6 +34,15 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
             test_model_score = r2_score(y_test,y_test_pred)
             report[list(models.keys())[i]] = train_model_score
         return report
+
+    except Exception as e:
+        raise CustomException(e,sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)
 
     except Exception as e:
         raise CustomException(e,sys)
